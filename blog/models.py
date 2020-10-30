855638cc532abd1897ed52a95f4ba384a1f71df6
nikhil <nikhilk3023@gmail.com>
2020-10-30 22:57:46 +0530

    updated links

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=255)
+    link=models.CharField(max_length=1000,default="")
     author=models.CharField(max_length=14)
     slug=models.CharField(max_length=130)
     timeStamp=models.DateTimeField(blank=True)
--- proj/models.py
+++ proj/models.py
@@ -4,6 +4,7 @@ class Projects(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=255)
     author=models.CharField(max_length=100)
+    link=models.CharField(max_length=1000,default="")
     timeStamp=models.DateTimeField(blank=True)
     content=models.TextField()
     image = models.ImageField(upload_to='proj/images', default="")
